38eb4d8c8b3ea745bc1fb8556cee3bac2ea7dbd4
todorovicd <dt9669@moval.edu>
2019-07-19 12:58:15 -0500

    initial commit - final project

---
+++ app.js
@@ -0,0 +1,33 @@
+const btn = document.querySelector(".talk"),
+  content = document.querySelector(".content");
+
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+
+recognition.onstart = () => {
+  console.log("voice recognition is activated, you can talk now");
+};
+
+recognition.onresult = event => {
+  const current = event.resultIndex;
+
+  const transcript = event.results[current][0].transcript;
+  content.textContent = transcript;
+};
+
+// add the listener to the btn
+
+btn.addEventListener("click", () => {
+  recognition.start();
+});
+
+function readOutLoud(message) {
+  const speech = new SpeechSynthesisUtterance();
+  speech.text = message;
+  speech.volume = 1;
+  speech.rate = 1;
+  speech.pitch = 1;
+
+  window.speechSynthesis.speak(speech);
+}
